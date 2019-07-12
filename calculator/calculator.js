4df1dedb429cafc3a18ce140fd3ba7d4c2098655
lmjcbs <liamdavidjacobs@gmail.com>
2019-07-12 18:21:34 +0200

    calculator tasks

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,33 @@
-function add () {
-	
-}
+/* eslint-disable require-jsdoc */
+const add = (...val) => val.reduce((a, c) => a + c);
 
-function subtract () {
-	
-}
+const subtract = (a, b) => a - b;
 
-function sum () {
-	
-}
+const sum = (val) => val.reduce(((a, c) => a + c), 0);
 
-function multiply () {
-	
-}
+const multiply = (val) => val.reduce(((a, c) => a * c), 1);
 
-function power() {
-	
-}
+const power = (b, p) => {
+  let total = b;
+  for (let i = 1; i < p; i += 1) {
+    total *= b;
+  }
+  return total;
+};
 
-function factorial() {
-	
-}
+const factorial = (n) => {
+  let total = 1;
+  for (let i = 1; i <= n; i += 1) {
+    total *= i;
+  }
+  return total;
+};
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
