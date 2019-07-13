f1d17f49ac51c61eb6e204133ccb19950611a1a0
lmjcbs <liamdavidjacobs@gmail.com>
2019-07-13 10:22:34 +0200

    palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function(str) {
+  const charsRegex = /[a-z]/;
+  const strArr = str.toLowerCase().split('');
+  const result = strArr.filter((char) => charsRegex.test(char));
+  return result.join('') === result.reverse().join('');
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
